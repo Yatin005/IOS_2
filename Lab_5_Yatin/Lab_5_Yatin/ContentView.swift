9d08ba86d2988f5ef7f98fb837d573a095889433
Het <hetshah1324@gmail.com>
2025-06-10 14:00:36 -0400

    commit

---
+++ Lab_5_Yatin/Lab_5_Yatin/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Lab_5_Yatin
+//
+//  Created by Deep Kaleka on 2025-06-10.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Lab_5_Yatin/Lab_5_Yatin/Lab_5_YatinApp.swift
@@ -0,0 +1,17 @@
+//
+//  Lab_5_YatinApp.swift
+//  Lab_5_Yatin
+//
+//  Created by Deep Kaleka on 2025-06-10.
+//
+
+import SwiftUI
+
+@main
+struct Lab_5_YatinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
